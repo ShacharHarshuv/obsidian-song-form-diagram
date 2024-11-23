b2b6bfbc72b6c17bfcf79f69aacd8795efca1e89
ShacharHarshuv <shachar.harshuv@gmail.com>
2024-11-22 23:35:34 -0500

    Fix colors

--- src/getSectionColor.ts
+++ src/getSectionColor.ts
@@ -1,8 +1,14 @@
+const introColor = "#6B7280";
+const chorusColor = "#EF4444";
+const bPartColor = "#F59E0B";
+const verseColor = "#0EA5E9";
+const bridgeColor = "#10B981";
+
 const colors = [
-	"#EF4444",
-	"#0EA5E9",
-	"#4ADE80",
-	"#F59E0B",
+	chorusColor,
+	verseColor,
+	bridgeColor,
+	bPartColor,
 	"#6366F1",
 	"#D946EF",
 	"#FACC15",
@@ -17,8 +23,32 @@ export function getSectionColor(name?: string | null) {
 		return colors[index++ % colors.length];
 	}
 
+	if (/\bA\b/.test(name) || name?.toLowerCase().includes("chorus")) {
+		return chorusColor;
+	}
+
+	if (name?.toLowerCase().includes("intro")) {
+		return introColor;
+	}
+
+	if (name?.toLowerCase().includes("verse")) {
+		return verseColor;
+	}
+
 	if (!namedSectionsColors.has(name)) {
 		namedSectionsColors.set(name, index++);
+	}
+
+	if (/\bB\b/.test(name)) {
+		return bPartColor;
+	}
+
+	if (
+		name?.toLowerCase().includes("bridge") ||
+		name?.toLowerCase().includes("c-part") ||
+		name?.toLowerCase().includes("c part")
+	) {
+		return bridgeColor;
 	}
 
 	return colors[namedSectionsColors.get(name)! % colors.length];
